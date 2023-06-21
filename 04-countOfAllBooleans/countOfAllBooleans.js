b9296a0525d5a1a78538f11f368717520113f5ad
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-06-21 18:24:25 -0400

    3-5 complete

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,13 @@
 function countOfAllBooleans(arr) {
-  // Insert code here;
+  let counter = 0;
+  for (let i = 0; i < arr.length; i++) {
+
+    if (arr[i] === true || arr[i] === false) {
+      counter += 1;
+    }
+
+  }
+  return counter;
 }
 
 // Do not edit this line;
--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,11 @@
 function countOfAllBooleansAndStrings(arr) {
-  // Insert code here;
+  let counter = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === true || arr[i] === false || typeof arr[i] === 'string') {
+      counter += 1;
+  }
+  }
+return counter;
 }
 
 // Do not edit this line;
