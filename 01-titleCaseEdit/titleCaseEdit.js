05be8675256426e0f6af18db9614edae2a59bd29
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-06-21 17:51:10 -0400

    0-2 complete

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,12 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  let holder = title.split(" ");
+
+  for (let i = 0; i < holder.length; i++) {
+    holder[i] = holder[i].charAt(0).toUpperCase() + holder[i].slice(1);
+  }
+  const newString = holder.join(" ");
+  return newString;
 }
 
 // Do not edit this line;
