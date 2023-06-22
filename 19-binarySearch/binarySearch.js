05d4404ab9354206842ff78aabd50f7825f24ce7
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-06-21 20:26:22 -0400

    binary search complete

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -3,17 +3,19 @@ class MySolution {
     this.flag = false; // this is optional to use;
   }
 
-  binarySearch(nums, target, start, end) {
-    if (start > end) return -1;
-    let mid = math.floor((start+end)/2);
+  binarySearch(nums, target, start = 0, end = nums.length-1) {
+    if (start > end) {
+      return false;
+    }
+    let mid = Math.floor((start+end)/2);
 
-    if (nums[mid] === target)
-      return true;
-
-    if (target < arr[mid])
-      return this.binarySearch(nums, target, start, mid - 1);
-      else
-      return this.binarySearch(nums, target, mid+1, end);
+    if (nums[mid] === target) {
+      return true
+    } else if (target < nums[mid]) {
+      return this.binarySearch(nums, target, start, mid-1);
+    } else {
+      return this.binarySearch(nums, target, mid+1, end)
+    }
   }
 }
 
