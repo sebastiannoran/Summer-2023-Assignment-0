6c290a05ff058e430b3bb491f491863e3bf24e68
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-06-21 20:58:33 -0400

    pairSum complete

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,6 +1,16 @@
 function pairSum(nums, target) {
-  // Insert code here;
-  //checking if githib push works -- empty space
+
+  if (nums.length <= 1) {
+    throw new Error("less than or equal to 1");
+  }
+  for (let i = 0; i < nums.length-1; i ++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 // Do not edit this line;
