38dc4e5c0df0006d04beccf9b03d79a2647d2c1e
sebastiannoran <119528708+sebastiannoran@users.noreply.github.com>
2023-06-21 21:27:32 -0400

    union of sets complete

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,14 @@
 function setUnionOfAnyAmountOfSets(...args) {
-  // Insert code here;
+    let result = new Set();
+
+    for (let i = 0; i < args.length; i++) {
+      let set = args[i];
+      let values = Array.from(set); // make an array based of set from arg[0], arg[1]...
+      for (let j = 0; j < values.length; j++) {
+        result.add(values[j]); // take values from array and place back into result
+      }
+    }
+    return result;
 }
 
 // Do not edit this line;
